991d97115a940c9f5f7487445c49ce5fff75df34
Marc El Achkar <Achkar@Marcs-MacBook-Pro.local>
2015-04-11 14:36:21 +0300

    initial commit of android app

---
+++ app/src/main/java/com/example/achkar/jaras/SeePost.java
@@ -0,0 +1,16 @@
+package com.example.achkar.jaras;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Achkar on 15-04-11.
+ */
+public class SeePost extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.seepost);
+    }
+
+
+}
